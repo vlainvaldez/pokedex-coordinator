1460e16473746a9206e94a950ba36c73471e1f60
alvin valdez <alvinjosephvaldez@yahoo.com>
2017-10-03 13:28:40 +0800

    added unowned in self to avoid retain cycle in closures

--- PokedexFFUF/Main/MainVC.swift
+++ PokedexFFUF/Main/MainVC.swift
@@ -12,8 +12,16 @@ import IGListKit
 
 final class Mainvc: JAViewController {
     
+    unowned var rootView: MainView { return self.view as! MainView}
+    unowned var collectionView: UICollectionView { return self.rootView.collectionView }
+    
+    
     override func loadView() {
-        self.view = MainView()        
+        self.view = MainView()
+    }
+    
+    override func viewDidLoad() {
         
     }
 }
+
--- PokedexFFUF/Main/MainView.swift
+++ PokedexFFUF/Main/MainView.swift
@@ -13,10 +13,25 @@ import IGListKit
 
 public final class MainView: JAView {
     
+    let collectionView: UICollectionView = {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        let collectionView: UICollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        return collectionView
+    }()
+    
+    
     public override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = UIColor.white
         
+        self.rpd.subview(forAutoLayout: self.collectionView)
+        
+        self.collectionView.snp.makeConstraints { [unowned self] (make: ConstraintMaker) -> Void in
+            make.top.equalTo(20.0)
+            make.leading.equalTo(self).offset(10.0)
+            make.trailing.equalTo(self).inset(10.0)
+            make.bottom.equalTo(self)
+        }
         
     }
     
