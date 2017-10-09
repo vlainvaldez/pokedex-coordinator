d293854fa443e25e8610675179e2b8a89103ca70
alvin valdez <alvinjosephvaldez@yahoo.com>
2017-10-09 15:14:56 +0800

    added a collectionview sections and cell for details screen

---
+++ PokedexFFUF/Detail/StatsSectionController.swift
@@ -0,0 +1,9 @@
+//
+//  StatsSectionController.swift
+//  PokedexFFUF
+//
+//  Created by FFUF on 09/10/2017.
+//  Copyright © 2017 FFUF. All rights reserved.
+//
+
+import Foundation
---
+++ PokedexFFUF/Detail/UIViews/StatsCell.swift
@@ -0,0 +1,9 @@
+//
+//  StatsCell.swift
+//  PokedexFFUF
+//
+//  Created by FFUF on 09/10/2017.
+//  Copyright © 2017 FFUF. All rights reserved.
+//
+
+import Foundation
