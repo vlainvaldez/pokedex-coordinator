81a08b5546cdad654395bbb81aae90f80e023d4d
alvin valdez <alvinjosephvaldez@yahoo.com>
2017-10-13 13:27:17 +0800

    not using the coordinator for info screen

---
+++ PokedexFFUF/Info/InfoView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoView.swift
+//  PokedexFFUF
+//
+//  Created by FFUF on 13/10/2017.
+//  Copyright © 2017 FFUF. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Rapid
+import IGListKit
+
+final class InfoView: JAView {
+    
+    
+    
+}
--- PokedexFFUF/Main/MainCoordinator.swift
+++ PokedexFFUF/Main/MainCoordinator.swift
@@ -11,6 +11,7 @@ import Rapid
 import FSwiftNetworking
 import FSwiftParser
 import BrightFutures
+import FFuFArchitecture
 
 public final class MainCoordinator: AbstractCoordinator {
     
@@ -41,13 +42,41 @@ public final class MainCoordinator: AbstractCoordinator {
 
 extension MainCoordinator: MainVCDelegate{
     public func infoClicked() {
-        let coordinator: InfoCoordinator = InfoCoordinator(navigationController: self.navigationController)
+        let vc: InfoVC = InfoVC(items: [
+            TextItem(text: "Lorem ipsum keme keme keme 48 years dites lulu ang planggana kirara guash cheapangga nang wasok borta kabog chopopo bonggakea na ang bakit bakit lorem ipsum keme keme mahogany at nang sudems thunder nakakalurky na ang kasi planggana at ang daki ang wrangler shonget otoko na ang jowabelles fayatollah kumenis at ang matod planggana doonek planggana sangkatuts mabaho juts daki shontis chipipay keme sa mahogany juts at bakit wasok na shonga , shonga-shonga jowa bokot sangkatuts bokot biway buya Gino Mike chopopo majubis ng shonget kasi shonga ang tanders chopopo bakit jowabelles Cholo tetetet oblation sa lorem ipsum keme keme juts ng pamenthol kabog sa majubis otoko shonget mabaho pranella jowabelles oblation bokot lulu jowabella mashumers na ang ang tamalis , klapeypey-klapeypey."),
+            // swiftlint:disable:next line_length
+            TextItem(text: "Lorem ipsum keme keme keme 48 years dites lulu ang planggana kirara guash cheapangga nang wasok borta kabog chopopo bonggakea na ang bakit bakit lorem ipsum keme keme mahogany at nang sudems thunder nakakalurky na ang kasi planggana at ang daki ang wrangler shonget otoko na ang jowabelles fayatollah kumenis at ang matod planggana doonek planggana sangkatuts mabaho juts daki shontis chipipay keme sa mahogany juts at bakit wasok na shonga , shonga-shonga jowa bokot sangkatuts bokot biway buya Gino Mike chopopo majubis ng shonget kasi shonga ang tanders chopopo bakit jowabelles Cholo tetetet oblation sa lorem ipsum keme keme juts ng pamenthol kabog sa majubis otoko shonget mabaho pranella jowabelles oblation bokot lulu jowabella mashumers na ang ang tamalis , klapeypey-klapeypey."),
+            ExpandableItem(
+                title: "A First Title",
+                items: [
+                    // swiftlint:disable:next line_length
+                    TextItem(text: "Lorem ipsum keme keme keme 48 years dites lulu ang planggana kirara guash cheapangga nang wasok borta kabog chopopo bonggakea na ang bakit bakit lorem ipsum keme keme mahogany at nang sudems thunder nakakalurky na ang kasi planggana at ang daki ang wrangler shonget otoko na ang jowabelles fayatollah kumenis at ang matod planggana doonek planggana sangkatuts mabaho juts daki shontis chipipay keme sa mahogany juts at bakit wasok na shonga , shonga-shonga jowa bokot sangkatuts bokot biway buya Gino Mike chopopo majubis ng shonget kasi shonga ang tanders chopopo bakit jowabelles Cholo tetetet oblation sa lorem ipsum keme keme juts ng pamenthol kabog sa majubis otoko shonget mabaho pranella jowabelles oblation bokot lulu jowabella mashumers na ang ang tamalis , klapeypey-klapeypey."),
+                    // swiftlint:disable:next line_length
+                    TextItem(text: "Lorem ipsum keme keme keme 48 years dites lulu ang planggana kirara guash cheapangga nang wasok borta kabog chopopo bonggakea na ang bakit bakit lorem ipsum keme keme mahogany at nang sudems thunder nakakalurky na ang kasi planggana at ang daki ang wrangler shonget otoko na ang jowabelles fayatollah kumenis at ang matod planggana doonek planggana sangkatuts mabaho juts daki shontis chipipay keme sa mahogany juts at bakit wasok na shonga , shonga-shonga jowa bokot sangkatuts bokot biway buya Gino Mike chopopo majubis ng shonget kasi shonga ang tanders chopopo bakit jowabelles Cholo tetetet oblation sa lorem ipsum keme keme juts ng pamenthol kabog sa majubis otoko shonget mabaho pranella jowabelles oblation bokot lulu jowabella mashumers na ang ang tamalis , klapeypey-klapeypey."),
+                    // swiftlint:disable:next line_length
+                    TextItem(text: "Lorem ipsum keme keme keme 48 years dites lulu ang planggana kirara guash cheapangga nang wasok borta kabog chopopo bonggakea na ang bakit bakit lorem ipsum keme keme mahogany at nang sudems thunder nakakalurky na ang kasi planggana at ang daki ang wrangler shonget otoko na ang jowabelles fayatollah kumenis at ang matod planggana doonek planggana sangkatuts mabaho juts daki shontis chipipay keme sa mahogany juts at bakit wasok na shonga , shonga-shonga jowa bokot sangkatuts bokot biway buya Gino Mike chopopo majubis ng shonget kasi shonga ang tanders chopopo bakit jowabelles Cholo tetetet oblation sa lorem ipsum keme keme juts ng pamenthol kabog sa majubis otoko shonget mabaho pranella jowabelles oblation bokot lulu jowabella mashumers na ang ang tamalis , klapeypey-klapeypey.")
+                ]
+            ),
+            ExpandableItem(
+                title: "A Second Title",
+                items: [
+                    // swiftlint:disable:next line_length
+                    TextItem(text: "Bacon ipsum dolor amet venison jowl turducken, swine tail chicken kielbasa ham hock sausage. Landjaeger chuck swine alcatra pancetta sausage. Ham ball tip jowl jerky short ribs turducken sirloin chicken doner andouille tongue cow. Brisket pork chop jerky chicken, pork loin tri-tip ground round venison kielbasa drumstick beef tail landjaeger ball tip."),
+                    // swiftlint:disable:next line_length
+                    TextItem(text: "Filet mignon strip steak ham, short ribs pastrami short loin tenderloin chicken rump frankfurter picanha ball tip cupim. Bresaola beef filet mignon chicken shoulder ham hock. Tail ribeye meatball t-bone short ribs kielbasa. Pastrami burgdoggen beef ribs frankfurter, picanha capicola ham hock bresaola rump t-bone ground round sirloin ham. Bacon picanha jowl drumstick short ribs biltong landjaeger kielbasa ribeye pork loin meatloaf sausage sirloin shankle. Pastrami meatball andouille short ribs shoulder alcatra porchetta hamburger ham.\n\nSausage bresaola meatball pig pork belly porchetta ham hock frankfurter hamburger pork loin meatloaf flank shoulder ground round t-bone. Jowl andouille ribeye pig t-bone. Alcatra pork loin shankle short ribs swine bacon shank flank turducken tongue. Cupim kevin tail frankfurter biltong sausage."),
+                    // swiftlint:disable:next line_length
+                    TextItem(text: "Bacon ipsum dolor amet venison jowl turducken, swine tail chicken kielbasa ham hock sausage. Landjaeger chuck swine alcatra pancetta sausage. Ham ball tip jowl jerky short ribs turducken sirloin chicken doner andouille tongue cow. Brisket pork chop jerky chicken, pork loin tri-tip ground round venison kielbasa drumstick beef tail landjaeger ball tip.\n\nFilet mignon strip steak ham, short ribs pastrami short loin tenderloin chicken rump frankfurter picanha ball tip cupim. Bresaola beef filet mignon chicken shoulder ham hock. Tail ribeye meatball t-bone short ribs kielbasa. Pastrami burgdoggen beef ribs frankfurter, picanha capicola ham hock bresaola rump t-bone ground round sirloin ham. Bacon picanha jowl drumstick short ribs biltong landjaeger kielbasa ribeye pork loin meatloaf sausage sirloin shankle. Pastrami meatball andouille short ribs shoulder alcatra porchetta hamburger ham.\n\nSausage bresaola meatball pig pork belly porchetta ham hock frankfurter hamburger pork loin meatloaf flank shoulder ground round t-bone. Jowl andouille ribeye pig t-bone. Alcatra pork loin shankle short ribs swine bacon shank flank turducken tongue. Cupim kevin tail frankfurter biltong sausage.\n\nDrumstick cupim shank boudin andouille. Bresaola flank pig capicola cow ribeye. Picanha shankle pancetta, short loin chuck porchetta t-bone pork belly tenderloin leberkas chicken jowl. Drumstick boudin salami, meatball cow sirloin cupim biltong meatloaf bacon shoulder corned beef frankfurter.\n\nRibeye drumstick pork belly tenderloin, swine bresaola corned beef. Pancetta pork chop porchetta bacon, landjaeger chuck filet mignon strip steak sausage tri-tip. Pig brisket chicken shankle. Pork loin rump pig andouille corned beef swine biltong turducken alcatra bresaola.\n\nDoes your lorem ipsum text long for something a little meatier? Give our generator a try… it’s tasty!")
+                ]
+            )
+            
+            ])
         
-        self.add(childCoordinator: coordinator)
+        vc.view.backgroundColor = UIColor.white
         
         DispatchQueue.main.async {
-            coordinator.start()
+            self.navigationController.pushViewController(vc, animated: true)
         }
+        
     }
     
     public func pokemonClicked(_ icon: PokemonIcon, view: MainView) {
